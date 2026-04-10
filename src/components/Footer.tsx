36c333862de7f460639d0a86903f176724aa584c
samanthablanco14 <blanco.samantha@outlook.com>
2026-04-10 16:01:06 -0400

    Change footer author and LinkedIn link
    Updated footer links to reflect new author information.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Footer.scss'
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../assets/styles/Footer.scss";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a
+          href="https://www.linkedin.com/in/samantha-blanco14/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>© Samantha Carolina Blanco Isea</p>
     </footer>
   );
 }
