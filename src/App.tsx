386a15fd8cda9c5a42133d59ee4e3de3858c6f1c
samanthablanco14 <blanco.samantha@outlook.com>
2026-04-11 21:01:29 -0400

    Refactor App component to remove dark mode
    Removed dark mode functionality and simplified the App component structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import React, {useState, useEffect} from "react";
-import {
-  Main,
-  Timeline,
-  Expertise,
-  Project,
-  Contact,
-  Navigation,
-  Footer,
-} from "./components";
-import FadeIn from './components/FadeIn';
-import './index.scss';
+import React from "react";
+import "./index.scss";
+import { Main, Navigation, Project, Timeline, Expertise, Contact, Footer } from "./components";
 
 function App() {
-  const [mode, setMode] = useState('light');
-
-    const handleModeChange = () => {
-        if (mode === 'dark') {
-            setMode('light');
-        } else {
-            setMode('dark');
-        }
-    }
-
-    useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-      }, []);
-
-    return (
-    <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
-        <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
-        <FadeIn transitionDuration={700}>
-            <Main/>
-            <Expertise/>
-            <Timeline/>
-            <Project/>
-            <Contact/>
-        </FadeIn>
-        <Footer />
+  return (
+    <div className="main-container light-mode">
+      <Navigation />
+      <Main />
+      <Expertise />
+      <Timeline />
+      <Project />
+      <Contact />
+      <Footer />
     </div>
-    );
+  );
 }
 
 export default App;
